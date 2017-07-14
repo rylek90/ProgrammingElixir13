2ede5c4bd630a4f1d8374d63fc3e53d1e23cbbaf
Maciek Rylko <rylko.maciej@gmail.com>
2017-07-14 13:58:40 +0200

    Add String.to_integer on count parameter

--- issues/test/cli_test.exs
+++ issues/test/cli_test.exs
@@ -9,4 +9,10 @@ defmodule CliTest do
     assert parse_args(["--help", "anything"]) == :help
   end
 
+  test "three values returned if three given", do:
+    assert parse_args(["user", "project", "99"]) == {"user", "project", 99}
+
+  test "count is defaulted if two values given", do:
+    assert parse_args(["user", "project"]) == {"user", "project", 4}
+
 end
