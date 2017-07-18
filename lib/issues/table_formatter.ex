685cf1eccea8056b97a8bcac9152e006f1416b4c
Maciek Rylko <rylko.maciej@gmail.com>
2017-07-18 14:16:21 +0200

    Table formatter fixes and tests

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -1,15 +1,14 @@
 defmodule Issues.TableFormatter do
-  import Enum, only: [each: 2, map: 2, map_join: 3, max: 1]
+  import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1]
 
   def print_table_for_columns(rows, headers) do
-    with data_by_columns = split_into_columns(rows, headers),
-      column_widths = widths_of(data_by_columns),
-      format = format_for(column_widths)
-    do
-      puts_one_line_in_columns(headers, format)
-      IO.puts(separator(column_widths))
-      puts_in_columns(data_by_columns, format)
-    end
+    data_by_columns = split_into_columns(rows, headers)
+    column_widths = widths_of(data_by_columns)
+    format = format_for(column_widths)
+
+    puts_one_line_in_columns(headers, format)
+    IO.puts separator(column_widths)
+    puts_in_columns(data_by_columns, format)
   end
 
   def split_into_columns(rows, headers) do
@@ -25,17 +24,16 @@ defmodule Issues.TableFormatter do
     for column <- columns, do: column |> map(&String.length/1) |> max
 
   def format_for(column_widths), do:
-    map_join(column_widths, "|", fn width -> "-#{width}s" end) <> "~n"
+    map_join(column_widths, " | ", fn width -> "~-#{width}s" end) <> "~n"
 
   def separator(column_widths), do:
     map_join(column_widths, "-+-", fn width -> List.duplicate("-", width) end)
 
-  def puts_in_columns(data_by_columns, format) do
+  def puts_in_columns(data_by_columns, format), do:
     data_by_columns
     |> List.zip
     |> map(&Tuple.to_list/1)
     |> each(&puts_one_line_in_columns(&1, format))
-  end
 
   def puts_one_line_in_columns(fields, format), do:
     :io.format(format, fields)
